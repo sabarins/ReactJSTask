8addafb7f27e155b380d616f78ffed1d7ce55b8c
Sabari S <ssabari1492000@gmail.com>
2025-09-17 00:29:45 +0530

    update signup.js

--- my-moviesapp/src/Components/Signup.js
+++ my-moviesapp/src/Components/Signup.js
@@ -136,7 +136,7 @@ function Signup() {
                 }}
               >
                 <Typography variant="h4" sx={{ fontWeight: "bold" }}>
-                  Welcome{" "}
+                  Welcome Sabari{" "}
                   <WavingHandIcon sx={{ color: "#ffde6f", fontSize: "35px" }} />
                 </Typography>
 
